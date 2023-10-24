5ef17a3be7307f86bb26fd280197bc191d3d19a8
jerzy szajner <jerzy.szajner@gmail.com>
2023-10-24 15:02:42 +0200

    Extend conditional logic to include 'papier' and 'nożyce' moves

--- Desktop/projekty/javascript-prework/js/script.js
+++ Desktop/projekty/javascript-prework/js/script.js
@@ -1,8 +1,13 @@
-var computerMove = 'kamień',
-    playerMove = 'papier',
-    randomNumber1 = Math.floor(Math.random() * 3 + 1),
-    randomNumber2 = Math.floor(Math.random() * 9 + 11);
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to '+ playerMove + ', to wygrywasz!');
-printMessage('Wylosowana liczba z zakresu 1-3 to: ' + randomNumber1);
-printMessage('Wylosowana liczba z zakresu 11-19 to: ' + randomNumber2);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else if (randomNumber == 3) {
+    computerMove = 'nożyce';
+} 
+
+printMessage('Mój ruch: ' + computerMove);
