f509d1eda5711727c62042dc2f2ea0e1f488591d
jerzy szajner <jerzy.szajner@gmail.com>
2023-10-24 11:14:47 +0200

    Add liczby losowe

--- Desktop/projekty/javascript-prework/js/script.js
+++ Desktop/projekty/javascript-prework/js/script.js
@@ -1,5 +1,8 @@
-var computerMove;
-var playerMove;
-computerMove = 'kamień';
-playerMove = 'papier';
-printMessage('Zagrałem '+ computerMove + '! Jeśli Twój ruch to '+ playerMove + ', to wygrywasz!');
+var computerMove = 'kamień',
+    playerMove = 'papier',
+    randomNumber1 = Math.floor(Math.random() * 3 + 1),
+    randomNumber2 = Math.floor(Math.random() * 9 + 11);
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to '+ playerMove + ', to wygrywasz!');
+printMessage('Wylosowana liczba z zakresu 1-3 to: ' + randomNumber1);
+printMessage('Wylosowana liczba z zakresu 11-19 to: ' + randomNumber2);
